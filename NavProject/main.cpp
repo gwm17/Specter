021358a620d6591a64f5c8c57c89ff8533e02fbe
Gordon McCann <gwm17@my.fsu.edu>
2022-01-19 22:47:20 -0500

    Update NavProject to reflect changes

--- NavProject/main.cpp
+++ NavProject/main.cpp
@@ -1,7 +1,6 @@
 #include "Navigator.h"
 
 Navigator::Application* Navigator::CreateApplication() { return new Application(); }
-Navigator::PhysicsEventBuilder* Navigator::CreatePhysicsEventBuilder() { return new PhysicsEventBuilder(); }
 
 int main(int argc, const char** argv)
 {
@@ -9,12 +8,8 @@ int main(int argc, const char** argv)
 	NAV_TRACE("Logger Initialized!");
 
 	auto app = Navigator::CreateApplication();
-	auto evb = Navigator::CreatePhysicsEventBuilder();
-	Navigator::Application::LinkHistogramMap();
-	Navigator::Application::LinkParameterList();
 
 	app->Run();
 
 	delete app;
-	delete evb;
 }
